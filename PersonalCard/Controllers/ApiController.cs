68797aa4da93dde9d7ef55de7de84ea5f4aa212f
Emelin Nikita <clever899@gmail.com>
2018-04-10 22:08:23 +0300

    SmartContractAdd Api begin

--- PersonalCard/Controllers/ApiController.cs
+++ PersonalCard/Controllers/ApiController.cs
@@ -14,14 +14,6 @@ using PersonalCard.Services;
 
 namespace PersonalCard.Controllers
 {
-    public class request_data
-    {
-        public string key;
-        public string target;
-        public string email;
-        public string password;
-    }
-
     public class request_info
     {
         public string code;
@@ -31,6 +23,7 @@ namespace PersonalCard.Controllers
     public class send_data
     {
         public List<Medical> medicals;
+        public List<Contract> contracts;
     }
 
     public class response_api
@@ -183,6 +176,81 @@ namespace PersonalCard.Controllers
             }
 
         }
+
+        public async Task<IActionResult> smartcontractadd([FromQuery]string hash_сustomer, [FromQuery]string hash_еxecutor, [FromQuery]string order_sum,
+            [FromQuery]string prepaid_expense, [FromQuery]string condition)
+        {
+            User user = null;
+            Contract contract = null;
+            Transactions transactions = null;
+                        try
+                        {
+                            string contract_hash = await ShaEncoder.GenerateSHA256String(order_sum + prepaid_expense + DateTime.Now);
+                            contract = new Contract
+                            {
+                                hash_сustomer = hash_сustomer,
+                                hash_еxecutor = hash_еxecutor,
+                                order_sum = Convert.ToInt32(order_sum) - Convert.ToInt32(prepaid_expense),
+                                prepaid_expense = prepaid_expense,
+                                is_freze = true,
+                                is_Done = false,
+                                contractID = contract_hash
+                            };
+                            //,Hash = await ShaEncoder.GenerateSHA256String(model.login + model.password + model.code_phrase
+                            string json = JsonConvert.SerializeObject(contract);
+                            blockchainService.AddBlockAsync(await blockchainService.generateNextBlockAsync(json, user.Hash, hash_еxecutor));
+                            transactions = new Transactions
+                            {
+                                original_wallet = hash_сustomer
+                                ,
+                                destination_wallet = hash_еxecutor,
+                                info = json,
+                                timestamp = DateTime.Now.ToString()
+                            };
+                            await _context.Transactions.AddAsync(transactions);
+                            await _context.SaveChangesAsync();
+                            user =await  _context.User.FirstOrDefaultAsync(u=> u.Hash == hash_еxecutor);
+                            user.balance = Convert.ToInt16(prepaid_expense);
+                            _context.User.Update(user);
+                            await _context.SaveChangesAsync();
+
+                            var data_response = new response_api();
+                            data_response.request_Info = new request_info();
+                            data_response.request_Info.answer = "OK";
+                            data_response.request_Info.code = "200";
+                            data_response.send_data = new send_data();
+                            List<Block> blocks = _context.Block.Where(u => u.wallet_hash == hash_сustomer && u.destination_wallet_hash == hash_еxecutor).ToList();
+
+                            List<Contract> contracts = new List<Contract>();
+                            foreach (var bloks in blocks)
+                            {
+                                contracts.Add(JsonConvert.DeserializeObject<Contract>(bloks.data));
+
+                            }
+                            data_response.send_data.contracts = contracts;
+                            var dt_resp = JsonConvert.SerializeObject(data_response);
+                            //var data_response2 = JsonConvert.DeserializeObject<response_api>(dt_resp);
+
+
+
+                            return Content(dt_resp, "application/json");
+                        }
+                        catch
+                        {
+                            var answ = new response_api();
+                            answ.request_Info = new request_info();
+                            answ.request_Info.code = "403";
+                            answ.request_Info.answer = "BadInfo";
+                            var dt_resp = JsonConvert.SerializeObject(answ);
+                            return Content(dt_resp, "application/json");
+
+                        }
+
+
+                    
+
+        }
+
     }
 }
         
